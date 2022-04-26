d06cd4811ae0bd8e90bcdf1fecccd63eb463b0b2
Andres Calzadias <acalzadias467@icloud.com>
2022-04-25 20:08:57 -0700

    last changes

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
-const container = document.getElementById("container");
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_GRID = 16;
+
+let currentColor = DEFAULT_COLOR;
+
+const container = document.getElementById("grid-container");
+let x = DEFAULT_GRID;
+let y = DEFAULT_GRID;
+
+// Functions for changing grid size
+// const xInput = document.getElementById("x");
+// x.addEventListener("xInput",updateX);
+// const yInput = document.getElementById("y");
+// y.addEventListener("yInput",updateY);
+// function updateX(e) {
+//     x = e.target.value;
+// }
+// function updateY(e) {
+//     y = e.target.value;
+// }
+
+// Will add an mouse click event listener
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+function color(){
+    e.target.style.backgroundColor = currentColor;
+}
 
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        // cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
 };
 
-makeRows(16, 16);
+// function colorSelection(color){
+//     // will need to take in color values 
+//     // assign color to event of drawing 
+// }
+
+function clear(){
+    
+}
+
+makeRows(x, y);
+
+// const divs = document.getElementsByClassName('grid-item');
+// const color = "#002b36";
+
+// colorSelection(color);
