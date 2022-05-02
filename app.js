24485f535902c6cad606c6801f51057ca1390c5e
Andres Calzadias <acalzadias467@icloud.com>
2022-05-02 09:42:27 -0700

    Added color pick functionality

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ makeRows(currentSize);
 
 // Clears The grid
 const btn = document.getElementById("clear");
-btn.addEventListener('click', clear);
+btn.addEventListener("click", clear);
 
 // Will add an mouse click event listener
 let mouseDown = false;
@@ -21,19 +21,27 @@ document.body.onmouseup = () => (mouseDown = false);
 
 // Paints with selected color
 function color(e){
+    const colorPicker = document.getElementById("color");
+    const highlight = document.getElementsByClassName("grid-item :hover");
+    
+    currentColor = colorPicker.value;
+    colorPicker.oninput = (e) => {
+        currentColor = color.value;
+    }
+
     if (e.type === 'mouseover' && !mouseDown)return
     e.target.style.backgroundColor = currentColor;
 }
 
 // Creates grid using arguments as size (argument^2)
 function makeRows(size) {
-    container.style.setProperty('--grid-rows', size);
-    container.style.setProperty('--grid-cols', size);
+    container.style.setProperty("--grid-rows", size);
+    container.style.setProperty("--grid-cols", size);
     for (let i = 0; i < (size * size); i++) {
         let cell = document.createElement("div");
         // cell.innerText = (i + 1);
-        cell.addEventListener('mouseover', color);
-        cell.addEventListener('mousedown', color);
+        cell.addEventListener("mouseover", color);
+        cell.addEventListener("mousedown", color);
         container.appendChild(cell).className = "grid-item";
     };
 };
@@ -46,6 +54,8 @@ slider.oninput = (e) => {
     makeRows(currentSize);
     clear();
 }
+
+
 
 // function colorSelection(color){
 //     // will need to take in color values 
