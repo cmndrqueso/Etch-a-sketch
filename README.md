5aacdfc360ec8ac2d7dba472102c92f16d6a6f4f
Andres Calzadias <acalzadias467@icloud.com>
2022-06-23 03:41:08 -0700

    Some fideling arround with a highlight function, non functional yet

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-sketch
+
+This is a project where I am creating a website where a user can draw. The user is able to select a color and size of the grid, then they can draw on the grid by clicking and dragging their mouse across the canvas.
--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const DEFAULT_GRID = 16;
 // Sets defaults
 let currentColor = DEFAULT_COLOR;
 let currentSize = DEFAULT_GRID;
+let highlightedColor = currentColor;
+let currentCellColor = "";
 
 //Creates the grid
 makeRows(currentSize);
@@ -29,7 +31,12 @@ function color(e){
 
     if (e.type === 'mouseover' && !mouseDown)return
     e.target.style.backgroundColor = currentColor;
-}
+};
+
+function highlight(e){
+    currentCellColor = e.getPropertyValue("background-color");
+    console.log(currentCellColor);
+};
 
 // Creates grid using arguments as size (argument^2)
 function makeRows(size) {
@@ -37,21 +44,12 @@ function makeRows(size) {
     container.style.setProperty("--grid-cols", size);
     for (let i = 0; i < (size * size); i++) {
         let cell = document.createElement("div");
-        // cell.innerText = (i + 1);
         cell.addEventListener("mouseover", color);
         cell.addEventListener("mousedown", color);
         container.appendChild(cell).className = "grid-item";
     };
 };
 
-// Takes values from the slider to create new grid
-const slider = document.getElementById("slider");
-currentSize = slider.value;
-slider.oninput = (e) => {
-    currentSize = slider.value;
-    makeRows(currentSize);
-    clear();
-}
 // const highlight = document.getElementById("grid-item");
 // highlight.addEventListener("mouseenter", e => {
 //     highlight.style.backgroundColor = currentColor;
@@ -59,13 +57,17 @@ slider.oninput = (e) => {
 // highlight.addEventListener("mouseleave",e => {
 //     highlight.style.backgroundColor = "";
 // });
+// Takes values from the slider to create new grid
+const slider = document.getElementById("slider");
+currentSize = slider.value;
+slider.oninput = (e) => {
+    currentSize = slider.value;
+    makeRows(currentSize);
+    clear();
+
+}
 
 
-
-// function colorSelection(color){
-//     // will need to take in color values 
-//     // assign color to event of drawing 
-// }
 
 // Clears the grid
 function clear(){
